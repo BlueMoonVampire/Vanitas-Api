31d58d087b2bf2829743d2d2011343e778098f96
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2022-01-01 16:20:11 +0530

    update to make db fast

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"syl-api/routes"
+	"syl-api/db"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,7 @@ import (
 func main() {
 
 	r := gin.Default()
+	db.Database()
 
 	r.GET("/user/:user", routes.Get_User)
 	r.GET("/", routes.Home)
